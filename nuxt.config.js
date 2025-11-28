485e8c45f504699329c799d8f91046d472651b5a
marcus1688 <148477064+marcus1688@users.noreply.github.com>
2025-11-28 22:53:17 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,12 @@ export default defineNuxtConfig({
       ],
       link: [
         {
+          rel: "preload",
+          href: "/images/loading.gif",
+          as: "image",
+          type: "image/gif",
+        },
+        {
           rel: "stylesheet",
           href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css",
         },
